ef1da88e1bb4e1f7a181c348ded409caee989d9c
hitzpro <wakhidaziz95192@gmail.com>
2026-03-22 09:07:09 +0700

    Update Package.json & astro.config.mjs yukNgaji

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
@@ -11,5 +12,6 @@ export default defineConfig({
   },
   server: {
     port: 1234,
-  }
+  },
+  adapter: vercel(),
 });
