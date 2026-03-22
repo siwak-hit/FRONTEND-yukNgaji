2ad51778ebf84f423128e6723af17e87515cef36
siwak-hit <wakhidkhoirulaziz.work@gmail.com>
2026-03-22 09:19:47 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'; // <--- UBAH BAGIAN INI (Hapus /serverless)
 
 export default defineConfig({
   output: 'server',
